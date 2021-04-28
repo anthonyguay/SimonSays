1c0cb222c1c29910b7a0828551093718a7c90310
Anthony Guay <anthonyguay@mac.com>
2021-04-28 16:46:14 -0700

    Project created

---
+++ Simon Says/Service/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//
+
+
+import Foundation
+import UIKit
+
+struct K {
+	
+	struct UI {
+		// Time waited before displaying alert windows
+		static let delayBeforeShowingAlertViews = 1.0
+		
+		// Cell Colors
+		static let validLocalMoveColor = UIColor.green
+		static let invalidLocalMoveColor = UIColor.red
+		static let validOpponentMoveColor = UIColor.blue
+		static let notSelectedColor = UIColor.lightGray
+	}
+	
+	
+	
+}
+
